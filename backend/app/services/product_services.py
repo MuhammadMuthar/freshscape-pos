295335a6f6a49547e71be1cb42b7bf26f5bcb160
Muhammad Muthar <mutharnoonari1776@gmail.com>
2026-07-22 11:46:29 +0500

    implement custom exceptions and product sevices

---
+++ backend/app/services/product_services.py
@@ -0,0 +1,66 @@
+from sqlalchemy.orm import Session
+
+from app.core.exceptions import (
+    BadRequestException,
+    ConflictException,
+)
+from app.repositories.category_repository import CategoryRepository
+from app.repositories.product_repository import ProductRepository
+from app.schemas.product import ProductCreate
+
+
+class ProductService:
+
+    def __init__(self):
+        self.product_repository = ProductRepository()
+        self.category_repository = CategoryRepository()
+
+    def create(
+        self,
+        db: Session,
+        product: ProductCreate,
+    ):
+        if self.product_repository.get_by_barcode(
+            db,
+            product.barcode,
+        ):
+            raise ConflictException(
+                "Barcode already exists."
+            )
+        
+        if self.product_repository.get_by_sku(
+            db,
+            product.sku,
+        ):
+            raise ConflictException(
+                "SKU already exists."
+            )
+        
+        if product.selling_price < product.cost_price:
+            raise BadRequestException(
+                "Selling price cannot be lower than cost price."
+            )
+
+        if product.stock_quantity < 0:
+            raise BadRequestException(
+                "Stock quantity cannot be negative."
+            )
+
+        if product.minimum_stock < 0:
+            raise BadRequestException(
+                "Minimum stock cannot be negative."
+            )
+
+        category = self.category_repository.get_by_id(
+            db,
+            product.category_id,
+        )
+
+        if category is None:
+            raise BadRequestException(
+                "Category does not exist."
+            )
+        return self.product_repository.create(
+            db,
+            product,
+        )
